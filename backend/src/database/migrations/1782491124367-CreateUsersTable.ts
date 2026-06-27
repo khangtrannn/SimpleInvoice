60aed795262a14db746b97aca10b824af3e044fb
Khang Tran <khangtrann8198@gmail.com>
2026-06-27 15:56:18 +0700

    fix(migrations): replace uuid_generate_v4() with gen_random_uuid()

--- backend/src/database/migrations/1782491124367-CreateUsersTable.ts
+++ backend/src/database/migrations/1782491124367-CreateUsersTable.ts
@@ -4,7 +4,7 @@ export class CreateUsersTable1782491124367 implements MigrationInterface {
     name = 'CreateUsersTable1782491124367'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "email" character varying(255) NOT NULL, "password_hash" character varying(255) NOT NULL, "fullname" character varying(255) NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "uq_users_email" UNIQUE ("email"), CONSTRAINT "pk_users" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "email" character varying(255) NOT NULL, "password_hash" character varying(255) NOT NULL, "fullname" character varying(255) NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "uq_users_email" UNIQUE ("email"), CONSTRAINT "pk_users" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
